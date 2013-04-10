a08dc813b74109987d2b39530b775f6faff56767
Frédéric Bolvin <frederic@bolvin.de>
2013-04-11 00:48:14 +0200

    Bugfixes

--- blockfile_incremental.rb
+++ blockfile_incremental.rb
@@ -18,14 +18,14 @@ class BlockfileIncremental
     @hash_table[@processed_bytes] = block_hash.to_s
   end
 
-  def self.load_hashtable
+  def self.load_hashtable(filename)
     # Loading Hashtable
     stored_hash_table = nil
-    if File.exist?('hashtable.dat')
+    if File.exist?("#{filename}_hashtable.dat")
       puts 'Loading stored hash table...'
       stored_hash_table = Hash.new
 
-      open('hashtable.dat') do |hash_table|
+      open("#{filename}_hashtable.dat") do |hash_table|
         hash_table.read.each_line do |line|
           end_block, checksum = line.chomp.split(";")
           stored_hash_table[end_block] = checksum.strip
@@ -36,22 +36,21 @@ class BlockfileIncremental
   end
 
   public
-  def self.incremental_backup
+  def self.incremental_backup(filename)
     time_begin = Time.now
     step_size_megaybtes = 5
     step_size = 1048576 * step_size_megaybtes
-    filename = '30M'
 
-    stored_hash_table = BlockfileIncremental.load_hashtable
+    stored_hash_table = BlockfileIncremental.load_hashtable (filename)
 
     puts "Processing #{filename}"
     puts "Size: #{File.size(filename)/1024/1024} MB"
 
     puts "\nReading..."
     @hash_table = Hash.new
-    @directory_name = DateTime.now.strftime('%Y%m%dT%H%M%S%z')
+    @directory_name = "#{filename}_#{DateTime.now.strftime('%Y%m%dT%H%M%S')}"
     Dir.mkdir(@directory_name)
-    File.open(filename) do | container |
+    File.open(filename, 'rb') do | container |
       current_block = String.new
       @processed_bytes = 0
       index = 0
@@ -85,7 +84,7 @@ class BlockfileIncremental
 
     # Writing
     puts "\nWrite hashtable..."
-    File.open('hashtable.dat', 'w') do | hash_file |
+    File.open("#{filename}_hashtable.dat", 'w') do | hash_file |
       @hash_table.each do | block_end, hash |
         hash_file.write("#{block_end};#{hash}\n")
       end
@@ -94,9 +93,9 @@ class BlockfileIncremental
     puts "Time passed: #{(Time.now - time_begin)} seconds"
   end
 
-  def self.restore_backup
+  def self.restore_backup(filename)
     time_begin = Time.now
-    stored_hash_table = BlockfileIncremental.load_hashtable
+    stored_hash_table = BlockfileIncremental.load_hashtable(filename)
     unless stored_hash_table.nil?
       directories = Dir.entries('.').select {|entry| File.directory? File.join('.',entry) and !(entry =='.' || entry == '..') }
 
@@ -122,5 +121,5 @@ class BlockfileIncremental
   end
 end
 
-#TruecryptIncremental.incremental_backup
-BlockfileIncremental.restore_backup
+BlockfileIncremental.incremental_backup('100M')
+#BlockfileIncremental.restore_backup('100M')
